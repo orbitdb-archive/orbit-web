7a816f5a59ba3ca88c423b9f8993d93cd16e9a84
Lauri Junkkari <latenssi@gmail.com>
2018-10-04 15:24:07 +0300

    Lint and format configs

--- src/config/ipfs-daemon.config.js
+++ src/config/ipfs-daemon.config.js
@@ -1,24 +1,22 @@
 // Ipfs daemon's default settings
-export function defaultIpfsDaemonSettings(ipfsDataDir) {
+export function defaultIpfsDaemonSettings (ipfsDataDir) {
   return {
     IpfsDataDir: ipfsDataDir,
     Addresses: {
       API: '/ip4/127.0.0.1/tcp/0',
-      Swarm: [
-        '/ip4/0.0.0.0/tcp/0'
-      ],
+      Swarm: ['/ip4/0.0.0.0/tcp/0'],
       Gateway: '/ip4/0.0.0.0/tcp/0'
     },
-    // How to use a local webrtc-star server: 
+    // How to use a local webrtc-star server:
     // https://github.com/libp2p/js-libp2p-webrtc-star
     // SignalServer: '0.0.0.0:9090', // localhost
     // SignalServer: '178.62.241.75', // old dev server
     SignalServer: 'star-signal.cloud.ipfs.team', // IPFS dev server
     API: {
       HTTPHeaders: {
-        "Access-Control-Allow-Origin": ['*'],
-        "Access-Control-Allow-Methods": ["PUT", "GET", "POST"],
-        "Access-Control-Allow-Credentials": ["true"]
+        'Access-Control-Allow-Origin': ['*'],
+        'Access-Control-Allow-Methods': ['PUT', 'GET', 'POST'],
+        'Access-Control-Allow-Credentials': ['true']
       }
     },
     Discovery: {
@@ -29,6 +27,6 @@ export function defaultIpfsDaemonSettings(ipfsDataDir) {
       webRTCStar: {
         Enabled: true
       }
-    },    
+    }
   }
 }
