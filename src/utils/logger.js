331e5c0b67efa734bb738e2a84ec4410e3e7f7fd
Lauri Junkkari <latenssi@gmail.com>
2018-10-10 15:29:02 +0300

    Improve debug enabling logic

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,8 @@
 function Logger () {}
 
-Logger.prototype.debug = process.env.NODE_ENV === 'development' ? console.log : () => {}
+const enableDebug = window.DEV || process.env.NODE_ENV === 'development'
+
+Logger.prototype.debug = enableDebug ? console.log : () => {}
 Logger.prototype.log = console.log
 Logger.prototype.info = console.info
 Logger.prototype.warn = console.warn
