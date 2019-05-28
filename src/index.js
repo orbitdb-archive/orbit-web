cea95d715d26644c155844d47091e5849f020c67
Jani Anttonen <jantto@utu.fi>
2019-05-28 13:06:31 +0300

    remove PWA capabilities

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import LoadAsync from './components/Loadable'
 
-import offlinePluginRuntime from 'offline-plugin/runtime'
-
 import { version } from '../package.json'
 
 const App = LoadAsync({
   loader: () => import(/* webpackChunkName: "App" */ './views/App')
 })
-
-if (process.env.NODE_ENV === 'production') {
-  offlinePluginRuntime.install()
-}
 
 render(<App />, document.getElementById('root'))
 
