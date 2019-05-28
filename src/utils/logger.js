1939eb83969eb63ca8820bacb0a74095eb3c032e
Jani Anttonen <jantto@utu.fi>
2019-05-28 11:09:34 +0300

    disable redundant logging in production

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,8 +5,8 @@ function Logger () {}
 const enableDebug = window.DEV || process.env.NODE_ENV === 'development'
 
 Logger.prototype.debug = enableDebug ? console.log : () => {}
-Logger.prototype.log = console.log
-Logger.prototype.info = console.info
+Logger.prototype.log = enableDebug ? console.log : () => {}
+Logger.prototype.info = enableDebug ? console.info : () => {}
 Logger.prototype.warn = console.warn
 Logger.prototype.error = console.error
 
