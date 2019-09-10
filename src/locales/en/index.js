99fe8c6a7ec6bfaaeaf9926e9c832af59e67fecf
Lauri Junkkari <latenssi@gmail.com>
2019-09-10 15:56:55 +0300

    Update deps and fix datefns

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -1,4 +1,4 @@
 import translation from './translation.json'
-import dateLocale from 'date-fns/locale/en'
+import dateLocale from 'date-fns/locale/en-US'
 
 export default { translation, dateLocale }
